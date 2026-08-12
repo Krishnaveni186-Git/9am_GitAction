06b50d7c7c57c2d0d2eacea8494cfcca86e8ec76
Krishnaveni Gadiya <Krishnanatalikcg@gmail.com>
2026-08-12 19:46:07 +0530

    Update iris_train_model.py

--- iris_train_model.py
+++ iris_train_model.py
@@ -12,7 +12,7 @@ sns.set(style='white')
 
 
 # Load Data
-dataset = pd.read_csv('iris.csv')
+from sklearn.datasets import load_iris
 
 # Feature names (Ensure no extra spaces or parentheses)
 dataset.columns = [colname.strip(' (cm)').replace(" ", "_") for colname in dataset.columns.tolist()]
